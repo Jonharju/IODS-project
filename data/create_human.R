ee67f9dcb7bc098f47ecd0b4f12eb07437946ec7
Jonas Harjunpää <jonas.harjunpaa@helsinki.fi>
2017-02-23 21:30:43 +0200

    wrangling done

--- data/create_human.R
+++ data/create_human.R
@@ -21,3 +21,19 @@ gii <- mutate(gii, F_m_working = F_working / M_working)
 human <- inner_join(hd, gii, by = "Country")
 
 write.csv(human, file = "human.csv", row.names=FALSE)
+human <- read.csv("human.csv", sep=",", header= T)
+
+#The second part starts here
+human <- mutate(human, GNI = as.numeric(human$GNI))
+
+keep_columns <- c("Country", "High_edu_f_m", "F_m_working", "Edu_exp", "Life_exp", "GNI", "Mat_deaths", "Young_births", "Parl_seats")
+human <- select(human, one_of(keep_columns))
+
+data.frame(human[-1], comp = complete.cases(human))
+human <- filter(human, complete.cases(human))
+
+human <- human[1:155, ]
+
+rownames(human) <- human$Country
+human <- dplyr::select(human, -Country)
+write.csv(human, file = "human.csv", row.names = FALSE)
